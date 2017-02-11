9a590842d5952fd58e6c0a388af980062dfa3f72
Ruud van Asseldonk <dev@veniogames.com>
2017-02-11 22:32:12 +0100

    Add script to GC all Git repos in a directory

---
+++ tools/gc-all-repos.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Usage: ./gc-all-repos.sh PATH [--aggressive]
+#
+# Runs 'git gc' in all directories in PATH which contain a (bare) Git
+# repository.
+
+if [ "$1" == "--aggressive" ]; then
+  git_args=$1
+  basedir=$2
+else
+  git_args=""
+  basedir=$1
+fi
+
+echo ":: Enumerating Git repositories in $basedir."
+
+for dir in $(find $basedir -maxdepth 1 -type d); do
+  # Check if the directory is a Git repository by running 'rev-parse --git-dir'.
+  if git -C $dir rev-parse --git-dir > /dev/null 2> /dev/null; then
+    echo ":: Entering $dir ..."
+    git -C $dir gc $git_args
+    echo ""
+  fi
+done
