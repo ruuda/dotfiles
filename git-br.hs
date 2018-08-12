1d3d9b6c712bde72a541cda962afd67137ec2b46
Ruud van Asseldonk <dev@veniogames.com>
2018-08-12 23:09:24 +0200

    Fail to derive monoid in git-br.hs
    This was the thing that made me expect that I could write this nicely in
    Haskell, but it turns out not to be possible, unfortunately.
    
    I want "Branch a" to be a monoid if "a" is, then I could first map every
    branch to "Branch [a]" by putting fields in singleton lists, and then I
    could foldMap them all together to get the columns. But alas, records in
    Haskell are not named tuples, this does not work.

--- git-br.hs
+++ git-br.hs
@@ -1,6 +1,23 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.5 script
+
+-- git-br.hs -- Git branch with tabular alignment.
+-- Written in 2018 by Ruud van Asseldonk.
+--
+-- To the extent possible under law, the author has dedicated all copyright and
+-- related neighbouring rights to this software to the public domain worldwide.
+-- See the CC0 dedication at https://creativecommons.org/publicdomain/zero/1.0/.
+
+-- Expects output from "git branch" with the following --format:
+--
+--     %(HEAD)%00%(objectname:short=7)%00%(refname)%00%(refname:short)%00
+--     %(upstream)%00%(upstream:short)%00%(upstream:track)
+--
+-- See also the alias line in my .gitconfig, and git-br.py
+
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 import qualified Data.Set as Set
 
@@ -13,6 +30,9 @@ data Branch a = Branch
   , upstreamShort :: a
   , upstreamTrack :: a
   } deriving (Eq, Ord, Show, Functor)
+
+deriving instance Semigroup a => Semigroup (Branch a)
+deriving instance Monoid a => Monoid (Branch a)
 
 parseBranch :: String -> Branch String
 parseBranch =
@@ -51,8 +71,9 @@ gitBr lines =
         then branch
         else branch { upstream = "" }
     branchTree = flatten "" $ getChildren (fmap fixUpstream branches) ""
+    columns = foldMap (fmap (:[])) branchTree
   in
-    fmap refnameShort $ branchTree
+    [show columns]
 
 main :: IO ()
 main = interact (unlines . gitBr . lines)
