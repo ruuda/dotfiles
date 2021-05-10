62474dc41adf726dd12299f55de08ab22add0f1c
Ruud van Asseldonk <dev@veniogames.com>
2021-05-10 15:36:00 +0200

    Ensure i2c kernel module is loaded for brightness

--- brightness.sh
+++ brightness.sh
@@ -7,6 +7,9 @@ if [ $UID != 0 ]; then
   exit 1
 fi
 
+# ddcutil requires this module.
+modprobe i2c-dev
+
 # Property 10 is brightness.
 ddcutil setvcp --display 1 10 $1
 ddcutil setvcp --display 2 10 $1
