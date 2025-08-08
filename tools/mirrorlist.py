d44f32d4937ce9f7d45c3b2c2694f025f0fc9073
Ruud van Asseldonk <dev@veniogames.com>
2025-08-08 19:50:12 +0200

    Filter mirrors using some basic mirror properties

--- tools/mirrorlist.py
+++ tools/mirrorlist.py
@@ -9,8 +9,9 @@
 
 import json
 
-from typing import Literal, List, NamedTuple
+from typing import Literal, List, NamedTuple, Set
 from http.client import HTTPSConnection
+from datetime import datetime, timedelta, timezone
 
 
 class Mirror(NamedTuple):
@@ -18,6 +19,7 @@ class Mirror(NamedTuple):
     The fields here map directly to those documented at
     <https://archlinux.org/mirrors/status/>.
     """
+
     active: bool
 
     # Completion, not a percentage, but between 0.0 and 1.0.
@@ -40,7 +42,7 @@ class Mirror(NamedTuple):
     ipv6: bool
     isos: bool
 
-    # Last
+    # Last time the mirror was synced, according to the Arch mirror checker.
     last_sync: str
 
     # Supported transport protocol.
@@ -53,6 +55,24 @@ class Mirror(NamedTuple):
     url: str
     details: str
 
+    def is_ok(self, country_codes: Set[str]) -> bool:
+        """Return whether we want this mirror in our final list."""
+        return (
+            self.active
+            and (self.protocol == "https")
+            and (self.country_code in country_codes)
+            and (self.completion_pct == 1.0)
+        )
+
+    def is_fresh(self, now: datetime) -> bool:
+        assert datetime.tzinfo is not None
+        dt = now - datetime.fromisoformat(self.last_sync)
+        # We require that the mirror is less than an hour behind on average,
+        # and that the last sync was less than 1.5 hours ago. Note, in practice
+        # both rule out some mirrors, there are mirrors that are in sync on
+        # average, but synced longer than 1.5h ago currently!
+        return (self.delay < 3600) and (dt < timedelta(minutes=90))
+
 
 def get_mirrors() -> List[Mirror]:
     timeout_seconds = 3.0
@@ -62,12 +82,24 @@ def get_mirrors() -> List[Mirror]:
     response = conn.getresponse()
     assert response.status == 200
     data = json.load(response)
-    mirrors: List[Mirror] = data["urls"]
+    return [Mirror(**mirror) for mirror in data["urls"]]
+
+
+def filter_mirrors(mirrors: List[Mirror]) -> List[Mirror]:
+    print(f"Start: {len(mirrors)} mirrors.")
+    countries = {"NL", "BE", "DE", "DK", "SE", "NO"}
+
+    now = datetime.now(timezone.utc)
+    mirrors = [m for m in mirrors if m.is_ok(countries) and m.is_fresh(now)]
+    print(f"Freshness: {len(mirrors)} mirrors.")
+
+    mirrors.sort(key=lambda m: m.duration_stddev)
+
     return mirrors
 
 
 def main() -> None:
-    mirrors = get_mirrors()
+    mirrors = filter_mirrors(get_mirrors())
     for m in mirrors:
         print(m)
 
